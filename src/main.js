9d786dec4da4c7fc42442d3b8508c4e6555a99cb
xzc516 <zxuu0151@student.monash.edu>
2025-09-01 14:40:57 +1000

    Initial commit with PrimeVue DataTable white background styling

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// import './assets/main.css'
+// import '@/assets/style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+// import DataTable from 'primevue/datatable'
+// import Column from 'primevue/Column'
+
+const app = createApp(App)
+app.use(PrimeVue, { 
+  theme: { 
+    preset: Aura,
+    options: {
+      darkModeSelector: 'none',
+      cssLayerOrder: 'tailwind-base, primevue, tailwind-utilities'
+    }
+  } 
+})
+
+// app.component('DataTable', DataTable)
+// app.component('Column', Column)
+
+app.mount('#app')
